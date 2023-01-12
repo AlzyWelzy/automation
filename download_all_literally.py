386e03fb7a6fd8dd2a165f78ac606869aa2356b6
AlzyWelzy <welzyalzy@gmail.com>
2023-01-12 15:45:15 +0530

    made it

--- download_all_literally.py
+++ download_all_literally.py
@@ -2,14 +2,23 @@ import requests
 import zipfile
 import io
 
+# GitHub access token
+access_token = "YOUR_ACCESS_TOKEN"
+
 # GitHub username
 username = "GITHUB_USERNAME"
 
 # GitHub REST API endpoint for getting a user's repositories
-url = f"https://api.github.com/users/{username}/repos"
+url = f"https://api.github.com/users/{username}/repos?type=all"
+
+# Headers for the API call, including the access token
+headers = {
+    "Authorization": f"Token {access_token}",
+    "Accept": "application/vnd.github+json",
+}
 
 # Make the API call
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 
 # Get the repository data from the response
 repositories = response.json()
@@ -21,8 +30,8 @@ for repo in repositories:
     download_url = repo["zip_url"]
 
     # Download the repository as a zip file
-    repo_zip = requests.get(download_url)
+    repo_zip = requests.get(download_url, headers=headers)
     with zipfile.ZipFile(io.BytesIO(repo_zip.content)) as zip_file:
         zip_file.extractall(repo_name)
 
-print(f"All public repositories of {username} have been downloaded.")
+print(f"All repositories of {username} have been downloaded.")
